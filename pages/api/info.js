c3730a43d6cff3bb4baa69caf25fdd5c0a7db0eb
Andrew Morton <a.morton@digitalmakers.io>
2021-03-22 12:02:54 +1000

    added /info api endpoint

---
+++ pages/api/info.js
@@ -0,0 +1,48 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+const BASE = process.env.URL || 'http://5200742d7a99.ngrok.io'
+
+export default (req, res) => {
+  res.status(200).json(
+    {
+      "blocks": [
+        {
+          "type": "context",
+          "elements": [
+            {
+              "type": "mrkdwn",
+              "text": "*ABN* 63 647 869 752"
+            }
+          ]
+        },
+        {
+          "type": "context",
+          "elements": [
+            {
+              "type": "mrkdwn",
+              "text": "*ACN* 647 869 752"
+            }
+          ]
+        },
+        {
+          "type": "context",
+          "elements": [
+            {
+              "type": "mrkdwn",
+              "text": "*TFN* Coming soon"
+            }
+          ]
+        },
+        {
+          "type": "context",
+          "elements": [
+            {
+              "type": "mrkdwn",
+              "text": "*Bank details* BSB: 062692 ACC: 4377 1135"
+            }
+          ]
+        }
+      ]
+    }
+  )
+}
